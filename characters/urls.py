044d7b72d011afcefb8f3ffb5689083bfd9e07cf
Maximus Champion <maximuschampion2002@gmail.com>
2025-06-02 14:02:50 +0300

    Added derf & 1st view - random character implemented

---
+++ characters/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from characters.views import get_random_characters_view
+
+
+app_name = "characters"
+urlpatterns = [
+    path("characters/random/", get_random_characters_view, name="characters_random")
+]
