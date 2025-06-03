89161821cdb584cc8dd8342a4bfd4327de24e6ce
Maximus Champion <maximuschampion2002@gmail.com>
2025-06-03 15:00:48 +0300

    fixed save_characters to handle integrity error(new characted) created only if npt exist with api_id in DB

--- characters/scraper.py
+++ characters/scraper.py
@@ -1,5 +1,6 @@
 import requests
 from django.conf import settings
+from django.db import IntegrityError
 from requests import Response
 
 from characters.models import Character
@@ -30,7 +31,10 @@ def scrape_characters() -> list[Character]:
 
 def save_character(characters: list[Character]) -> None:
     for character in characters:
-        character.save()
+        try:
+            character.save()
+        except IntegrityError:
+            print(f"User with 'api_id': {character.api_id} already exists")
 
 
 def sync_characters_with_api() -> None:
